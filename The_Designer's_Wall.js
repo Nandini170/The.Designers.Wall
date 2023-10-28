ef5cbe4628a0cb7df511a10ed98357eab21ae504
Nandini170 <125524886+Nandini170@users.noreply.github.com>
2023-10-28 12:08:13 +0530

    Update The_Designer's_Wall.js

--- The_Designer's_Wall.js
+++ The_Designer's_Wall.js
@@ -2,8 +2,8 @@ const alertmsg = document.getElementById("home");
 setTimeout(function () {
     alert("Hower over the images");
 
-}, 2000);
+}, 5000);
 setTimeout(function () {
     alert("After Howering, To access additional photos of the dress, click on the dress title.");
 
-}, 5000);
+}, 6000);
