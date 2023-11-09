1d03b50f1dfb63473c6661b52c8788d91bdcc415
Nandini170 <125524886+Nandini170@users.noreply.github.com>
2023-11-09 11:56:04 +0530

    Update The_Designer's_Wall.js

--- The_Designer's_Wall.js
+++ The_Designer's_Wall.js
@@ -2,8 +2,13 @@ const alertmsg = document.getElementById("home");
 setTimeout(function () {
     alert("Hower over the images");
 
-}, 5000);
+}, 4000);
 setTimeout(function () {
     alert("After Howering, To access additional photos of the dress, click on the dress title.");
 
-}, 6000);
+}, 5000);
+
+setTimeout(function () {
+    alert("This Site is made for Desktop screen only, for better experience please open in Desktop Mode");
+
+}, 1000);
